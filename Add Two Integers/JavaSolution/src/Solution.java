f5c3b34f6753f697d1fdd8336bc06e1192e1b24f
katkes <keshank1@hotmail.com>
2024-06-22 19:24:16 -0400

    Add Two Integers
    Java solution, O(1) time and space complexity

---
+++ Add Two Integers/JavaSolution/src/Solution.java
@@ -0,0 +1,20 @@
+
+public class Solution {
+
+	/**
+	 * Approach: Pretty self explanatory, the return is the addition of the two
+	 * 			 integers.
+	 * 
+	 * Time complexity: O(1) --> Only one operation is performed no matter of input size.
+	 * 
+	 * Space complexity: O(1) --> Done in place.
+	 * 
+	 * 
+	 * @param num1
+	 * @param num2
+	 * @return
+	 */
+	public int sum(int num1, int num2) {
+		return num1 + num2;
+	}
+}
