aab6237c1f0ea9ff8e609b3e76019ab3656ecaef
Keshan Kathiripilay <ke_kathi@live.concordia.ca>
2025-05-11 16:28:55 -0400

    Soliton for Three Consecutive Odds

---
+++ Three Consecutive Odds/JavaSolution/Solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public boolean threeConsecutiveOdds(int[] arr){
+
+        int count = 0;
+
+        for (int num : arr){
+            if (num % 2 == 1){
+                count++;
+                if (count  == 3) return true;
+            }
+            else {
+                count = 0;
+            
+            }
+        }
+
+        return false;
+
+    }
+} 
